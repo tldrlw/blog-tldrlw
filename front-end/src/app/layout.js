39b5b5879bf0a4c1aa3a32bc9412beac892c7df6
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-09-10 12:05:50 -0400

    saving this branch to reference in first blog post

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "blog.tldrlw.com",
-  description: "learn about tech powering *.tldrlw.com",
+  title: "title",
+  description: "description",
 };
 
 export default function RootLayout({ children }) {
