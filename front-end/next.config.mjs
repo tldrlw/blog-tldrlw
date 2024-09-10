630a7acd27b6cfa30d92803f7942279b5f7f1159
Refayat Haque <refayathaque@Refayats-MacBook-Pro.local>
2024-09-10 19:27:41 -0400

    updated dockerfile to copy over the public directory which has the images

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  // distDir: 'dist',
-  // output: 'standalone',
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
