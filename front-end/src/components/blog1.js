a96a6034e20cd1b58f8600860f556340bb5f224b
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-09-10 20:59:26 -0400

    done for the night, fr

--- front-end/src/components/blog1.js
+++ front-end/src/components/blog1.js
@@ -3,10 +3,10 @@ import CodeBlock from '@/components/codeBlock';
 const providerTf = `
 terraform {
   backend "s3" {
-    bucket         = "tfstate-tldrlw-blog"
+    bucket         = "<your s3 bucket name>"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tfstate-tldrlw-blog"
+    dynamodb_table = "<your dynamodb table name>"
     encrypt        = true
   }
   required_providers {
@@ -47,11 +47,11 @@ export default function Blog1() {
       <section className='mb-2 text-sm text-gray-700 md:mb-4 md:text-base'>
         <p className='mb-2 text-gray-700 md:mb-4'>
           This guide will take a very barebones next.js app and deploy it to
-          ECS, I assume that you have a basic understanding of Terraform,
-          setting up an IAM User with a Secret Key and Secret Access Key to run
-          Terraform code against your AWS account, and a basic understanding of
-          CI/CD pipelines written in YAML. Code references for everything we
-          will do can be found{' '}
+          ECS, I assume you have a basic understanding of Terraform, setting up
+          an IAM User with a Secret Key and Secret Access Key to run Terraform
+          code against your AWS account, and a basic understanding of CI/CD
+          pipelines written in YAML. Code references for everything we will do
+          can be found{' '}
           <a
             className='text-blue-500 hover:underline'
             href='https://github.com/tldrlw/blog-tldrlw/tree/boilerplate-nextjs'
@@ -69,13 +69,14 @@ export default function Blog1() {
             {' '}
             this
           </a>{' '}
-          guide to get that done. You can do it through the AWS managment
+          guide to get that done. You can do this through the AWS managment
           console or do it using Terraform, in my case, I did it using Terraform
-          and have the code only available locally, to provision the S3 bucket
+          and have the code only available locally. To provision the S3 bucket
           and the DynamoDB table I used the Terraform local state as opposed to
-          the remote backend. After you've created the remote backend resources,
-          you'll need to reference them in the remote backend you'll be setting
-          up for this project. Your provider.tf should look like this:
+          the remote backend. After you&apos;ve created the remote backend
+          resources, you&apos;ll need to reference them in the remote backend
+          you&apos;ll be setting up for this project. Your provider.tf should
+          look like this:
         </p>
         <CodeBlock
           filePath={'provider.tf'}
