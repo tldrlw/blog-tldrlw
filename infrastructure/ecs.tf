04a91d1ed8cd28a3b9a1a2d70a6aa4bb694e8245
*.tldrlw.com <162769166+tldrlw@users.noreply.github.com>
2024-09-30 18:23:30 -0400

    Update ecs.tf
    same reason as update to alb.tf

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -3,23 +3,21 @@ resource "aws_ecs_cluster" "main" {
 }
 
 module "ecs_service" {
-  source                      = "git::https://github.com/tldrlw/terraform-modules.git//ecs-service?ref=dev"
+  source                      = "git::https://github.com/tldrlw/terraform-modules.git//ecs-service"
   app_name                    = var.APP_NAME
   ecr_repo_url                = aws_ecr_repository.main.repository_url
   image_tag                   = var.IMAGE_TAG
   ecs_cluster_id              = aws_ecs_cluster.main.id
   task_count                  = 1
-  alb_target_group_arn        = module.main.alb_target_group_arn
+  alb_target_group_arn        = module.main.alb_target_group_arns[0]
   source_security_group_id    = module.main.alb_security_group_id
   security_group_egress_cidrs = ["0.0.0.0/0"]
   subnets                     = aws_subnet.public[*].id
   vpc_id                      = aws_vpc.main.id
   container_port              = 3000
   host_port                   = 3000
-  # linux_arm64          = true
-  # ^ set to true if using the following scripts to build and push images to ECR on M-series Macs:
-  # https://github.com/tldrlw/radiotodaydhaka/blob/main/docker-push.sh
-  # https://github.com/tldrlw/gc-reservations/blob/main/front-end/docker-push.sh
+  # linux_arm64                 = true
+  # ^ set to true if building and pushing images to ECR on M-series Macs: https://github.com/tldrlw/blog-tldrlw/blob/boilerplate-nextjs/front-end/docker-push.sh
 }
 # rm -rf .terraform/modules > terraform init
 # run ^ after pushing up changes to modules when testing locally
