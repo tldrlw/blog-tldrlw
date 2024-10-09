17305fbcc5229a2f515daf3b12e47a6d594446bc
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-10-09 12:10:25 -0400

    finished blog 4

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,11 +1,9 @@
-import Blog3 from '@/components/blog3';
-// import Blog4 from '@/components/blog3';
+import Blog4 from '@/components/blog4';
 
 export default function Home() {
   return (
     <main>
-      <Blog3></Blog3>
-      {/* <Blog4></Blog4> */}
+      <Blog4></Blog4>
     </main>
   );
 }
