f1b9da9e6ba90e82810d7a8610676dad8e70d14b
Refayat Haque <refayathaque@Refayats-MacBook-Pro.local>
2024-11-02 15:36:17 -0400

    setting up promtail to push cloudwatch logs to loki

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -52,10 +52,9 @@ module "ecs_service_loki" {
 }
 
 module "ecs_service_grafana" {
-  source   = "git::https://github.com/tldrlw/terraform-modules.git//ecs-service?ref=dev"
-  app_name = "grafana-tldrlw"
-  # ecr_repo_url                = "grafana/grafana"
-  ecr_repo_url                = aws_ecr_repository.grafana_with_awscli.repository_url
+  source                      = "git::https://github.com/tldrlw/terraform-modules.git//ecs-service?ref=dev"
+  app_name                    = "grafana-tldrlw"
+  ecr_repo_url                = aws_ecr_repository.grafana.repository_url
   image_tag                   = "latest"
   ecs_cluster_id              = aws_ecs_cluster.main.id
   task_count                  = 1
@@ -80,13 +79,13 @@ module "ecs_service_grafana" {
   iam_user_for_container_shell = "local"
   linux_arm64                  = true
   # ^ set to true if building and pushing images to ECR on M-series Macs:
-  # since building and pushing (locally) a custom grafana image with awscli installed (see infrastructure/grafana-with-awscli-image.sh)
+  # since building and pushing (locally) a custom grafana image (see infrastructure/grafana-docker.sh)
 }
 
 # rm -rf .terraform/modules > terraform init
 # run ^ after pushing up changes to modules when testing locally
 
-# useful aws cli commands to run shell commands inside running container
+# useful example aws cli commands to run shell commands inside a running container
 # # Fetch the task ID
 # TASK_ID=$(aws ecs list-tasks \
 #   --cluster main \
---
+++ infrastructure/promtail-ecs.tf
@@ -0,0 +1,79 @@
+resource "aws_security_group" "promtail_sg" {
+  name        = "promtail-sg"
+  description = "Security group for Promtail container"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow all outbound traffic so Promtail can send logs to Loki over HTTPS
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to any IP
+  }
+
+  # No inbound rules needed as Promtail does not need to receive any external connections
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [] # No inbound traffic allowed
+  }
+}
+
+resource "aws_ecs_service" "promtail" {
+  name            = "promtail-tldrlw"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.promtail.arn
+  desired_count   = 1 # Adjust this based on your needs
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = aws_subnet.public[*].id             # List of subnet IDs
+    security_groups  = [aws_security_group.promtail_sg.id] # Security group ID(s)
+    assign_public_ip = true                                # Set to false if you don't need a public IP
+    # Public IP Needed: If your Promtail container needs to communicate with external resources (like an external Loki endpoint over the internet) and your VPC doesn’t have a NAT gateway, then you should assign a public IP.
+    # No Public IP Needed: If your Promtail container communicates only with resources within the VPC (like a Loki container running in the same VPC) or your VPC has a NAT gateway that handles outbound traffic.
+    # should look into removing public access to loki later...
+  }
+}
+
+resource "aws_ecs_task_definition" "promtail" {
+  family                   = "promtail-tldrlw"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.promtail_task_execution_role.arn # ECS execution role for logging and image pull
+  task_role_arn            = aws_iam_role.promtail_task_role.arn           # Task role for application access
+  memory                   = "512"
+  cpu                      = "256"
+  container_definitions = jsonencode([
+    {
+      name      = "promtail"
+      image     = "${aws_ecr_repository.promtail.repository_url}:latest" # Replace with your custom Promtail image
+      cpu       = 256
+      memory    = 512
+      essential = true
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/promtail"
+          awslogs-region        = data.aws_region.current.name
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      environment = [
+        { name = "AWS_REGION", value = data.aws_region.current.name },
+        { name = "TEST", value = "test" }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "promtail-positions"
+          containerPath = "/tmp"
+          readOnly      = false
+        }
+      ]
+    }
+  ])
+  # Use an ephemeral volume suitable for Fargate
+  volume {
+    name = "promtail-positions"
+  }
+}
