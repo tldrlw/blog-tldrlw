a7a806ec3f56f6405f4d6e556e1d95504b9f3da0
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-11-29 16:03:44 -0500

    woot

--- infrastructure/vpn-client.tf
+++ infrastructure/vpn-client.tf
@@ -1,11 +1,14 @@
-# module "vpn_client" {
-#   source              = "git::https://github.com/tldrlw/terraform-modules.git//vpn-client?ref=dev"
-#   CLIENT_CIDR         = "10.1.0.0/22"
-#   DOMAIN              = "tldrlw.com"
-#   PRIVATE_SUBNET_CIDR = "10.0.20.0/24" # Adjusted range for the single VPN subnet
-#   ROUTE_53_ZONE_ID    = data.aws_route53_zone.tldrlw_com.zone_id
-#   TRUSTED_CIDR        = "100.15.211.96/32"
-#   VPC_CIDR            = "10.0.0.0/16"
-#   VPC_ID              = aws_vpc.main.id
-# }
+module "vpn_client" {
+  source              = "git::https://github.com/tldrlw/terraform-modules.git//vpn-client?ref=dev"
+  NAME                = "vpc-access"
+  PROJECT             = "tldrlw"
+  CLIENT_CIDR         = "10.1.0.0/22"
+  PRIVATE_SUBNET_CIDR = "10.0.20.0/24" # Adjusted range for the single VPN subnet
+  REGION              = "us-east-1"
+  ROUTE_53_ZONE_ID    = data.aws_route53_zone.tldrlw_com.zone_id
+  TRUSTED_CIDR        = "100.15.211.96/32"
+  VPC_CIDR            = "10.0.0.0/16"
+  VPC_ID              = aws_vpc.main.id
+  VPN_INACTIVE_PERIOD = "300" # seconds
+}
 # rm -rf .terraform/modules
