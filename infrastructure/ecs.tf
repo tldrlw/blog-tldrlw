1b4752e89f08bc5eeb91b62f929b10dd5b261250
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-12-21 11:17:50 -0500

    destroyed ecs service

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -8,24 +8,24 @@ resource "aws_ecs_cluster" "main" {
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster#attribute-reference
 
-module "ecs_service" {
-  source                      = "git::https://github.com/tldrlw/terraform-modules.git//ecs-service"
-  APP_NAME                    = var.APP_NAME
-  ecr_repo_url                = aws_ecr_repository.main.repository_url
-  image_tag                   = var.IMAGE_TAG
-  ecs_cluster_id              = aws_ecs_cluster.main.id
-  ECS_CLUSTER_NAME            = "main"
-  alb_target_group_arn        = module.main.alb_target_group_arns[0]
-  source_security_group_id    = module.main.alb_security_group_id
-  security_group_egress_cidrs = ["0.0.0.0/0"]
-  subnets                     = aws_subnet.public[*].id
-  vpc_id                      = aws_vpc.main.id
-  container_port              = 3000
-  host_port                   = 3000
-  # cpu                         = "256"
-  # memory                      = "512"
-  # ^ doubled both on 9/18/24, halved again (module default values) on 10/14/24
-}
+# module "ecs_service" {
+#   source                      = "git::https://github.com/tldrlw/terraform-modules.git//ecs-service"
+#   APP_NAME                    = var.APP_NAME
+#   ecr_repo_url                = aws_ecr_repository.main.repository_url
+#   image_tag                   = var.IMAGE_TAG
+#   ecs_cluster_id              = aws_ecs_cluster.main.id
+#   ECS_CLUSTER_NAME            = "main"
+#   alb_target_group_arn        = module.main.alb_target_group_arns[0]
+#   source_security_group_id    = module.main.alb_security_group_id
+#   security_group_egress_cidrs = ["0.0.0.0/0"]
+#   subnets                     = aws_subnet.public[*].id
+#   vpc_id                      = aws_vpc.main.id
+#   container_port              = 3000
+#   host_port                   = 3000
+#   # cpu                         = "256"
+#   # memory                      = "512"
+#   # ^ doubled both on 9/18/24, halved again (module default values) on 10/14/24
+# }
 
 # rm -rf .terraform/modules > terraform init
 # run ^ after pushing up changes to modules when testing locally
